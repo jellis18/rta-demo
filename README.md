1c0016faf034982bc87da8886f21c2370c3d0a48
Justin Ellis <justin.ellis18@gmail.com>
2022-04-17 13:56:51 -0400

    Adding app code

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ terraform apply -auto-approve
 ```bash
 az aks get-credentials --resource-group rta-resource-group --name rta-demo-cluster
 ```
+
+## Docker
+```bash
+docker build -t rtaacr.azurecr.io/rta-app:0.0.1 -f docker/Dockerfile .
+docker run -it --rm -p 8080:8080 rtaacr.azurecr.io/rta-app:0.0.1
+
+az acr login -n rtaacr
+docker image push rtaacr.azurecr.io/rta-app:0.0.1
+```
--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+import spacy
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from spacy import displacy
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def process_text(text: str) -> str:
+    doc = nlp(text)
+    rendered_html = displacy.render(doc, style="ent", minify=True)
+    if "<script" in rendered_html:
+        raise RuntimeError("May have malicious code, Aborting")
+    return rendered_html
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -10,3 +25,9 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.post("/extraction", response_class=HTMLResponse)
+async def extract(request: Request, text: str = Form(...)):
+    entities = process_text(text)
+    return templates.TemplateResponse("extraction.html", {"request": request, "entities": entities})
